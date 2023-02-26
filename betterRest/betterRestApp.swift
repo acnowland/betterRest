956bb9c11288bf1f358193e49c062d74f8d8ee4a
Adam Nowland <acnowland@gmail.com>
2023-02-26 13:56:02 -0800

    Initial Commit

---
+++ betterRest/betterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  betterRestApp.swift
+//  betterRest
+//
+//  Created by Adam Nowland on 2/26/23.
+//
+
+import SwiftUI
+
+@main
+struct betterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
